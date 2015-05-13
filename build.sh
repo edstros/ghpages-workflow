7ebf55db51a99750bca4c04ac23ea9dced84bf42
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-05-13 15:06:48 -0500

    Adds bower to gitignore,  modifies build.sh and package.json

--- build.sh
+++ build.sh
@@ -2,17 +2,33 @@
 
 # clean and prepare public directory
 rm -rf public
-mkdir public
+cp -r src public
 
 # compile jade to html
 ./node_modules/.bin/jade src -P
 cd src
-find . -name "*.html" | cpio -pdvm ../public # cpio takes argument and moves html to public
+find . -name "*.html" | cpio -pdvm ../public
 cd ..
-rm -rf src/*.html src/**/*.html public/**/_*.html public/_partials
+rm -rf src/*.html \
+       src/**/*.html \
+       public/**/_*.html \
+       public/_partials
 
-#compile sass to css
+# compile sass to css
 ./node_modules/.bin/node-sass \
   --output-style compressed \
   --source-map-embed \
   src/_styles/main.scss public/css/main.css
+
+# convert ES6 JS to ES5
+./node_modules/.bin/babel src --out-dir public -s inline
+
+# concat bower_components to public/lib
+./node_modules/.bin/bowcat . -o public/lib -m
+
+# clean unneeded files
+rm -rf public/_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
