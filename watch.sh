9f4025f22f02a1bdf86acb69159aa6b9f9c3ca29
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-05-14 13:48:29 -0500

    edits jade compiler for watch

--- watch.sh
+++ watch.sh
@@ -9,17 +9,23 @@ rm -rf public
 cp -r src public
 
 # compile jade to html
-./node_modules/.bin/jade src -o public -PH
-rm -rf public/_partials
+./node_modules/.bin/nodemon -e jade --watch src --exec "
+  ./node_modules/.bin/jade src -o public -PH
+  rm -rf src/*.html public/_partials" &
 
 # compile sass to css
 ./node_modules/.bin/node-sass \
   --output-style compressed \
   --source-map-embed \
-  src/_styles/main.scss public/css/main.css
+  --recursive \
+  src/_styles/main.scss public/css/main.css &
 
 # convert ES6 JS to ES5
-./node_modules/.bin/babel src --out-dir public -s inline
+./node_modules/.bin/babel \
+  src \
+  --out-dir public \
+  -s inline \
+  -w &
 
 # concat bower_components to public/lib
 if [ -d "bower_components" ]; then
@@ -32,3 +38,5 @@ rm -rf public/_styles \
        public/**/*.jade \
        public/*.scss \
        public/**/*.scss
+
+echo "Watching for changes..."
